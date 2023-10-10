d1841e336e463dfd13570f11a6691ca9b03ccdc4
Noa Dudai <nooninana@gmail.com>
2023-10-10 18:12:18 +0300

    checking out to a new branch

--- Models/Workers_week_schedule.py
+++ Models/Workers_week_schedule.py
@@ -5,3 +5,7 @@ from Models.Day_schedule_model import DayScheduleModel
 class WorkersWeekScheduleModel:
     def __init__(self, days_in_the_week: List[DayScheduleModel]):
         self.week = days_in_the_week
+        self.solutions = []
+
+    def add_solution(self, solution: List[DayScheduleModel]):
+        self.solutions.append(solution)
